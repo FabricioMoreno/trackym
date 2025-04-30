71841a259c1fece19806df3fddf0712bce7761ba
FabricioMoreno <fabricio.morenonaranjo@hotmail.com>
2025-04-30 03:05:28 -0500

    Another workout added

--- src/assets/infoTest.js
+++ src/assets/infoTest.js
@@ -71,9 +71,66 @@ const infoTest = {
             //   }
             ]
           },
+          {
+            "id": 2,
+            "nombre": "Rutina Hombros + Brazos",
+            "timer_sets_default": 1,
+            "timer_rest_default": 3,
+            "exercices": [
+              {
+                "name": "Press de hombro",
+                "muscular_group": ["Hombros"],
+                "sets_default": 4,
+                "reps_default": 12,
+                "weight_default": 35,
+                "completed_sets": []
+              },
+              {
+                "name": "Elevaciones laterales",
+                "muscular_group": ["Hombros"],
+                "sets_default": 4,
+                "reps_default": 12,
+                "weight_default": 20,
+                "completed_sets": []
+              },
+              {
+                "name": "Elevaciones frontales",
+                "muscular_group": ["Hombros"],
+                "sets_default": 4,
+                "reps_default": 12,
+                "weight_default": 20,
+                "completed_sets": []
+              },
+              {
+                "name": "Extensión de tríceps unilateral",
+                "muscular_group": ["Tríceps"],
+                "sets_default": 4,
+                "reps_default": 12,
+                "weight_default": 9,
+                "completed_sets": []
+              },
+              {
+                "name": "Extensión de tríceps sobre la cabeza",
+                "muscular_group": ["Tríceps"],
+                "sets_default": 4,
+                "reps_default": 12,
+                "weight_default": 24,
+                "completed_sets": []
+              },
+              {
+                "name": "Curl martillo bajo",
+                "muscular_group": ["Bíceps"],
+                "sets_default": 4,
+                "reps_default": 12,
+                "weight_default": 29,
+                "completed_sets": []
+              }
+            ]
+          }
 
         ]
       },
+
 
     ]
   }
